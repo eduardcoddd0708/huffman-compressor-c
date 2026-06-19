cf59e938308e8fbbe484b5838a0c982c9cb3178a
eduardcoddd0708 <codescuedward@gmail.com>
2026-06-20 00:57:51 +0300

    Define Node structure and function declaration in huffman.h

--- huffman.h
+++ huffman.h
@@ -1,1 +1,15 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
 
+// Node structure for the Huffman tree
+typedef struct Node {
+    char character;         // The character stored
+    unsigned int frequency; // Number of occurrences in the file
+    struct Node *left;      // Pointer to the left child
+    struct Node *right;     // Pointer to the right child
+} Node;
+
+// Function declarations
+void calculate_frequencies(const char *filename, unsigned int *frequencies);
+
+#endif
