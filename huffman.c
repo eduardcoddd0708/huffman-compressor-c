5b3971bec4639256c7664d644b09541faa5e186d
eduardcoddd0708 <codescuedward@gmail.com>
2026-06-20 00:58:36 +0300

    Implement frequency calculation from file
    Added a function to read a file and count character frequencies.

--- huffman.c
+++ huffman.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "huffman.h"
 
+// Function to read a file and count the frequency of each character
+void calculate_frequencies(const char *filename, unsigned int *frequencies) {
+    // Open the file in read mode
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error: Cannot open file %s\n", filename);
+        exit(1); // Exit the program if the file doesn't exist
+    }
+
+    int ch;
+    // Read the file character by character until End Of File (EOF)
+    while ((ch = fgetc(file)) != EOF) {
+        // Increment the frequency at the index corresponding to the ASCII value
+        frequencies[ch]++;
+    }
+
+    // Always close the file when done
+    fclose(file);
+}
