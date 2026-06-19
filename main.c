e6d5ffe663da9560701059e3cc56aa55270cd51e
eduardcoddd0708 <codescuedward@gmail.com>
2026-06-20 00:58:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include "huffman.h"
 
+int main() {
+    // Array to store the frequency of 256 possible ASCII characters
+    // Initialize all elements to 0
+    unsigned int frequencies[256] = {0}; 
+    
+    // For testing, create a simple text file named "test.txt" in your folder
+    const char *filename = "test.txt";
+
+    calculate_frequencies(filename, frequencies);
+
+    // Print characters that appear at least once
+    printf("Character Frequencies:\n");
+    for (int i = 0; i < 256; i++) {
+        if (frequencies[i] > 0) {
+            // Print special characters nicely (e.g., newline)
+            if (i == '\n') {
+                printf("'\\n' : %u\n", frequencies[i]);
+            } else {
+                printf("'%c'  : %u\n", i, frequencies[i]);
+            }
+        }
+    }
+
+    return 0;
+}
